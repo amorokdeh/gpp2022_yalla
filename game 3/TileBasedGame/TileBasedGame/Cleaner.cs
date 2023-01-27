83a0f0a66c8a3cec7443af4e55339c4786ee4877
amorokdeh <58468955+amorokdeh@users.noreply.github.com>
2023-01-27 10:51:35 +0100

    Neue Bilder

--- game 3/TileBasedGame/TileBasedGame/Cleaner.cs
+++ game 3/TileBasedGame/TileBasedGame/Cleaner.cs
@@ -10,7 +10,17 @@ namespace TileBasedGame
     {
         public void clean() {
 
-            
+            Program.Game._objects.clearObjects();
+            Program.Game._rendering.clearObjects();
+            Program.Game._physics.clearObjects();
+            Program.Game._controls.clearObjects();
+            Program.Game._collisions.clearObjects();
+            Program.Game._ai.clearObjects();
+            Program.Game._animations.clearObjects();
+            Program.Game._pool.clearObjects();
+            Program.Game._shootings.clearObjects();
+            Program.Game._updates.clearObjects();
+            Program.Game._maps.clearObjects();
         }
     }
 }
